992b79fb54022ff273e0c6fe19acb571177daca6
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-17 18:30:20 -0500

    Finished implementing training loop

--- python/run_q3.py
+++ python/run_q3.py
@@ -15,60 +15,21 @@ valid_x, valid_y = valid_data['valid_data'], valid_data['valid_labels']
 # plt.imshow(ex1)
 # plt.show()
 
-# Network configuration
+
 max_iters = 50
+# pick a batch size, learning rate
 batch_size = 50
-learning_rate = 1e-6
+learning_rate = 1e-2
 in_size = train_x[0].shape[0]
 hidden_size = 64
 out_size = train_y[0].shape[0]
-# Create network
+# Shuffle
+batches = get_random_batches(train_x,train_y,batch_size)
+batch_num = len(batches)
+# initialize layers here
 params = {}
 initialize_weights(in_size,hidden_size,params,'layer1')
 initialize_weights(hidden_size,out_size,params,'output')
-# Create mini-batches
-batches = get_random_batches(train_x,train_y,5)
-# Training
-for itr in range(max_iters):
-    total_loss = 0
-    avg_acc = 0
-    batch_num = 0
-    # batches = get_random_batches(x,y,5)
-    for xb,yb in batches:
-        pass
-        # forward
-        h1 = forward(xb,params,'layer1')
-        probs = forward(h1,params,'output',softmax)
-        # loss
-        # be sure to add loss and accuracy to epoch totals 
-        loss, acc = compute_loss_and_acc(yb, probs)
-        total_loss += loss
-        avg_acc = (acc + avg_acc*batch_num)/(batch_num+1)
-        batch_num += 1
-        # backward
-        delta1 = probs-yb
-        delta2 = backwards(delta1,params,'output',linear_deriv)
-        backwards(delta2,params,'layer1',sigmoid_deriv)
-        # apply gradient
-        params['Wlayer1'] = params['Wlayer1']-params['grad_Wlayer1']*learning_rate#/xb.shape[0]
-        params['blayer1'] = params['blayer1']-params['grad_blayer1']*learning_rate#/xb.shape[0]
-        params['Woutput'] = params['Woutput']-params['grad_Woutput']*learning_rate#/xb.shape[0]
-        params['boutput'] = params['boutput']-params['grad_boutput']*learning_rate#/xb.shape[0]
-
-
-        
-    if itr % 100 == 0:
-        print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,avg_acc))
-
-batches = get_random_batches(train_x,train_y,batch_size)
-batch_num = len(batches)
-
-params = {}
-
-# initialize layers here
-##########################
-##### your code here #####
-##########################
 
 
 # with default settings, you should get loss < 150 and accuracy > 80%
@@ -77,11 +38,24 @@ for itr in range(max_iters):
     total_acc = 0
     for xb,yb in batches:
         pass
-        # training loop can be exactly the same as q2!
-        ##########################
-        ##### your code here #####
-        ##########################
+        # forward
+        h1 = forward(xb,params,'layer1')
+        probs = forward(h1,params,'output',softmax)
+        # loss
+        loss, acc = compute_loss_and_acc(yb, probs)
+        total_loss += loss
+        total_acc += acc
+        # backward
+        delta1 = probs-yb
+        delta2 = backwards(delta1,params,'output',linear_deriv)
+        backwards(delta2,params,'layer1',sigmoid_deriv)
+        # apply gradient
+        params['Wlayer1'] -= params['grad_Wlayer1']*learning_rate#/xb.shape[0]
+        params['blayer1'] -= params['grad_blayer1']*learning_rate#/xb.shape[0]
+        params['Woutput'] -= params['grad_Woutput']*learning_rate#/xb.shape[0]
+        params['boutput'] -= params['grad_boutput']*learning_rate#/xb.shape[0]
 
+    total_acc /= batch_size
     if itr % 2 == 0:
         print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,total_acc))
 
