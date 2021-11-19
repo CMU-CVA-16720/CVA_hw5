f55fdc341bc1da282988a8608fbbb8a96367cd75
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-19 01:16:22 -0500

    Implemented rows extraction

--- python/run_q4.py
+++ python/run_q4.py
@@ -21,20 +21,64 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 for img in os.listdir('../images'):
+    # Get image and bounding boxes
     im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
     bboxes, bw = findLetters(im1)
+    
+    # Display image + bboxes
+    if False:
+        plt.imshow(bw)
+        for bbox in bboxes:
+            minr, minc, maxr, maxc = bbox
+            rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                    fill=False, edgecolor='red', linewidth=2)
+            plt.gca().add_patch(rect)
+        plt.show()
 
-    plt.imshow(bw)
+    # Get median height
+    height_array = []
     for bbox in bboxes:
         minr, minc, maxr, maxc = bbox
-        rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                fill=False, edgecolor='red', linewidth=2)
-        plt.gca().add_patch(rect)
-    plt.show()
-    # find the rows using..RANSAC, counting, clustering, etc.
-    ##########################
-    ##### your code here #####
-    ##########################
+        height_array.append(maxr-minr)
+    height_median = np.median(height_array)
+    
+    # Compute rows
+    rows = []
+    cur_row = []
+    last_minr = 0
+    for bbox in bboxes:
+        minr, minc, maxr, maxc = bbox
+        # First letter special case
+        if(last_minr == 0):
+            # Put first letter in row and move on
+            cur_row.append(bbox)
+            last_minr = minr
+            continue
+        delta_minr = abs(minr - last_minr)
+        # New row if delta(minr) exceeds median height
+        if(delta_minr > height_median):
+            # New row
+            rows.append(cur_row)
+            cur_row = []
+            cur_row.append(bbox)
+        else:
+            # Continue current row
+            cur_row.append(bbox)
+        last_minr = minr
+    # Append last row
+    rows.append(cur_row)
+
+    # Sort within rows for correct ordering
+    for row in rows:
+        row.reverse()
+    # Crop, tranpose and flatten
+    for row in rows:
+        for bbox in row:
+            minr, minc, maxr, maxc = bbox
+            plt.imshow(bw[minr:maxr,minc:maxc])
+            plt.show()
+    print("blah")
+
 
 
     # crop the bounding boxes
@@ -44,13 +88,13 @@ for img in os.listdir('../images'):
     ##### your code here #####
     ##########################
     
-    # load the weights
-    # run the crops through your neural network and print them out
-    import pickle
-    import string
-    letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
-    params = pickle.load(open('q3_weights.pickle','rb'))
-    ##########################
-    ##### your code here #####
-    ##########################
+    # # load the weights
+    # # run the crops through your neural network and print them out
+    # import pickle
+    # import string
+    # letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
+    # params = pickle.load(open('q3_weights.pickle','rb'))
+    # ##########################
+    # ##### your code here #####
+    # ##########################
     
