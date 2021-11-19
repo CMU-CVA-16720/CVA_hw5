54ffc6f3812b30464f8f4edf891b47388fd07009
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-18 21:46:52 -0500

    Added momentum GD, viz, logging & graphing

--- python/run_q5.py
+++ python/run_q5.py
@@ -14,6 +14,7 @@ train_x = train_data['train_data']
 valid_x = valid_data['valid_data']
 
 max_iters = 100
+# max_iters = 1
 # pick a batch size, learning rate
 batch_size = 36 
 learning_rate =  3e-5
@@ -32,6 +33,16 @@ initialize_weights(in_size,hidden_size,params,'layer1')
 initialize_weights(hidden_size,hidden_size,params,'layer2')
 initialize_weights(hidden_size,hidden_size,params,'layer3')
 initialize_weights(hidden_size,out_size,params,'output')
+# Initialize momentums
+params['m_Wlayer1'] = np.zeros(params['Wlayer1'].shape)
+params['m_blayer1'] = np.zeros(params['blayer1'].shape)
+params['m_Wlayer2'] = np.zeros(params['Wlayer2'].shape)
+params['m_blayer2'] = np.zeros(params['blayer2'].shape)
+params['m_Wlayer3'] = np.zeros(params['Wlayer3'].shape)
+params['m_blayer3'] = np.zeros(params['blayer3'].shape)
+params['m_Woutput'] = np.zeros(params['Woutput'].shape)
+params['m_boutput'] = np.zeros(params['boutput'].shape)
+
 
 # should look like your previous training loops
 train_loss_log, train_acc_log = [],[]
@@ -62,21 +73,58 @@ for itr in range(max_iters):
         delta3 = backwards(delta2,params,'layer3',relu_deriv)
         delta4 = backwards(delta3,params,'layer2',relu_deriv)
         backwards(delta4,params,'layer1',relu_deriv)
-        # apply gradient
-        params['Wlayer1'] -= params['grad_Wlayer1']*learning_rate
-        params['blayer1'] -= params['grad_blayer1']*learning_rate
-        params['Wlayer2'] -= params['grad_Wlayer2']*learning_rate
-        params['blayer2'] -= params['grad_blayer2']*learning_rate
-        params['Wlayer3'] -= params['grad_Wlayer3']*learning_rate
-        params['blayer3'] -= params['grad_blayer3']*learning_rate
-        params['Woutput'] -= params['grad_Woutput']*learning_rate
-        params['boutput'] -= params['grad_boutput']*learning_rate
-
+        # update momentum
+        params['m_Wlayer1'] = 0.9 * params['m_Wlayer1'] - params['grad_Wlayer1']*learning_rate
+        params['m_blayer1'] = 0.9 * params['m_blayer1'] - params['grad_blayer1']*learning_rate
+        params['m_Wlayer2'] = 0.9 * params['m_Wlayer2'] - params['grad_Wlayer2']*learning_rate
+        params['m_blayer2'] = 0.9 * params['m_blayer2'] - params['grad_blayer2']*learning_rate
+        params['m_Wlayer3'] = 0.9 * params['m_Wlayer3'] - params['grad_Wlayer3']*learning_rate
+        params['m_blayer3'] = 0.9 * params['m_blayer3'] - params['grad_blayer3']*learning_rate
+        params['m_Woutput'] = 0.9 * params['m_Woutput'] - params['grad_Woutput']*learning_rate
+        params['m_boutput'] = 0.9 * params['m_boutput'] - params['grad_boutput']*learning_rate
+        # update parameters
+        params['Wlayer1'] += params['m_Wlayer1']
+        params['blayer1'] += params['m_blayer1']
+        params['Wlayer2'] += params['m_Wlayer2']
+        params['blayer2'] += params['m_blayer2']
+        params['Wlayer3'] += params['m_Wlayer3']
+        params['blayer3'] += params['m_blayer3']
+        params['Woutput'] += params['m_Woutput']
+        params['boutput'] += params['m_boutput']
+    # logging
+    train_loss_log.append(total_loss)
+    # Printing
     if itr % 2 == 0:
         print("itr: {:02d} \t loss: {:.2f}".format(itr,total_loss))
     if itr % lr_rate == lr_rate-1:
         learning_rate *= 0.9
-        
+# Visualize results
+if False:
+    from mpl_toolkits.axes_grid1 import ImageGrid
+    for i in range(0,xb.shape[0]):
+        fig = plt.figure(figsize=(4., 4.))
+        grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                        nrows_ncols=(1, 2),  # creates 2x2 grid of axes
+                        axes_pad=0.1,  # pad between axes in inch.
+                        )
+        for indx,ax in enumerate(grid):
+            # Iterating over the grid returns the Axes.
+            if(indx == 0):
+                img = np.reshape(xb[i,:],(32,32))
+            else:
+                img = np.reshape(probs[i,:],(32,32))
+            ax.imshow(img)
+        plt.show()
+# Graphs
+if False:
+    ax = plt.axes()
+    ax.plot(np.arange(0,max_iters), train_loss_log, color='red') # training loss
+    plt.xlim(0, max_iters)
+    plt.title("Training Loss vs Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.show()
+
 # Q5.3.1
 import matplotlib.pyplot as plt
 # visualize some results
