2bfb6bb7d7053f0ff0b8ca2843eaaf17f0f7b465
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-18 19:36:09 -0500

    Finished implementing findLetters

--- python/q4.py
+++ python/q4.py
@@ -17,7 +17,7 @@ from skimage.restoration import denoise_bilateral
 
 # takes a color image
 # returns a list of bounding boxes and black_and_white image
-def findLetters(image, display=False):
+def findLetters(image, display=False, minsize = 100):
     # Output setup
     bboxes = []
     bw = None
@@ -32,27 +32,28 @@ def findLetters(image, display=False):
     cleared = clear_border(bw)
     # label image regions
     label_image = label(cleared)
-    # to make the background transparent, pass the value of `bg_label`,
-    # and leave `bg_color` as `None` and `kind` as `overlay`
-    image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
-    print("Regions detected: {}".format(len(regionprops(label_image))))
+    for i,region in enumerate(regionprops(label_image)):
+        # take regions with large enough areas
+        if region.area >= minsize:
+            # print("{}: Region size: {}".format(i+1,region.area))
+            bboxes.append(region.bbox)
+    # Display image with bounding boxes
     if(display):
+        # to make the background transparent, pass the value of `bg_label`,
+        # and leave `bg_color` as `None` and `kind` as `overlay`
+        image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
         fig, ax = plt.subplots(figsize=(10, 6))
+        # ax.imshow(label_image)
         ax.imshow(image_label_overlay)
-
-        for region in regionprops(label_image):
-            # take regions with large enough areas
-            if region.area >= 100:
-                # draw rectangle around segmented coins
-                minr, minc, maxr, maxc = region.bbox
-                rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                        fill=False, edgecolor='red', linewidth=2)
-                ax.add_patch(rect)
-
+        for cur_bbox in bboxes:
+            # draw rectangle around segmented coins
+            minr, minc, maxr, maxc = cur_bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                    fill=False, edgecolor='red', linewidth=2)
+            ax.add_patch(rect)
         ax.set_axis_off()
         plt.tight_layout()
         plt.show()
-
     return bboxes, bw
 
 if __name__ == "__main__":
