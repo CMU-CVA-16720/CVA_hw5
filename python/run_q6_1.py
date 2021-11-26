0c561b1cfab93c062ab553f908efa41f39b5ea62
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-26 11:07:17 -0500

    Rewrote training loop

--- python/run_q6_1.py
+++ python/run_q6_1.py
@@ -1,174 +1,99 @@
-# -*- coding: utf-8 -*-
-"""
-Training a Classifier
-=====================
-This is it. You have seen how to define neural networks, compute loss and make
-updates to the weights of the network.
-Now you might be thinking,
-What about data?
-----------------
-Generally, when you have to deal with image, text, audio or video data,
-you can use standard python packages that load data into a numpy array.
-Then you can convert this array into a ``torch.*Tensor``.
--  For images, packages such as Pillow, OpenCV are useful
--  For audio, packages such as scipy and librosa
--  For text, either raw Python or Cython based loading, or NLTK and
-   SpaCy are useful
-Specifically for vision, we have created a package called
-``torchvision``, that has data loaders for common datasets such as
-ImageNet, CIFAR10, MNIST, etc. and data transformers for images, viz.,
-``torchvision.datasets`` and ``torch.utils.data.DataLoader``.
-This provides a huge convenience and avoids writing boilerplate code.
-For this tutorial, we will use the CIFAR10 dataset.
-It has the classes: ‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘deer’,
-‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’. The images in CIFAR-10 are of
-size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
-.. figure:: /_static/img/cifar10.png
-   :alt: cifar10
-   cifar10
-Training an image classifier
-----------------------------
-We will do the following steps in order:
-1. Load and normalize the CIFAR10 training and test datasets using
-   ``torchvision``
-2. Define a Convolutional Neural Network
-3. Define a loss function
-4. Train the network on the training data
-5. Test the network on the test data
-1. Load and normalize CIFAR10
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-Using ``torchvision``, it’s extremely easy to load CIFAR10.
-"""
 import torch
+from torch.utils.data.dataset import TensorDataset
 import torchvision
 import torchvision.transforms as transforms
-
-########################################################################
-# The output of torchvision datasets are PILImage images of range [0, 1].
-# We transform them to Tensors of normalized range [-1, 1].
-
-########################################################################
-# .. note::
-#     If running on Windows and you get a BrokenPipeError, try setting
-#     the num_worker of torch.utils.data.DataLoader() to 0.
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-batch_size = 4
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-########################################################################
-# Let us show some of the training images, for fun.
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-# functions to show an image
-
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
-
-
-########################################################################
-# 2. Define a Convolutional Neural Network
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Copy the neural network from the Neural Networks section before and modify it to
-# take 3-channel images (instead of 1-channel images as it was defined).
-
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
+import scipy.io
 
+from nn import *
+
+# Batch size
+batch_size = 50
+
+# Epochs
+max_iters = 50
+# max_iters = 1
+
+# Learning rate & momentum
+learning_rate = 1e-1
+momentum = 0.9
+
+# Get data
+train_data = scipy.io.loadmat('../data/nist36_train.mat')
+valid_data = scipy.io.loadmat('../data/nist36_valid.mat')
+test_data = scipy.io.loadmat('../data/nist36_test.mat')
+train_x, train_y = train_data['train_data'], train_data['train_labels']
+valid_x, valid_y = valid_data['valid_data'], valid_data['valid_labels']
+test_x, test_y = test_data['test_data'], test_data['test_labels']
+# Convert to tensors - training
+train_x_tensor, train_y_tensor = torch.from_numpy(train_x).type(torch.float32), torch.from_numpy(train_y)
+trainset = TensorDataset(train_x_tensor, train_y_tensor)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,shuffle=True, num_workers=2)
+# Convert to tensors - validation
+valid_x_tensor, valid_y_tensor = torch.from_numpy(valid_x).type(torch.float32), torch.from_numpy(valid_y)
+validset = TensorDataset(valid_x_tensor, valid_y_tensor)
+valid_loader = torch.utils.data.DataLoader(validset, batch_size=batch_size,shuffle=True, num_workers=2)
+# Convert to tensors - testing
+test_x_tensor, test_y_tensor = torch.from_numpy(test_x).type(torch.float32), torch.from_numpy(test_y)
+testset = TensorDataset(test_x_tensor, test_y_tensor)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,shuffle=True, num_workers=2)
+
+# Network size
+in_size = train_x.shape[1]
+hidden_size = 64
+out_size = train_y.shape[1]
+
+# Shuffle
+batches = get_random_batches(train_x,train_y,batch_size)
+batch_num = len(batches)
+
+# Define neural net
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(in_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, out_size)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.sigmoid(self.fc1(x))
+        x = F.softmax(self.fc2(x))
         return x
-
-
 net = Net()
 
-########################################################################
-# 3. Define a Loss function and optimizer
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Let's use a Classification Cross-Entropy loss and SGD with momentum.
-
-import torch.optim as optim
-
+# Loss & trainer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
 
-########################################################################
-# 4. Train the network
-# ^^^^^^^^^^^^^^^^^^^^
-#
-# This is when things start to get interesting.
-# We simply have to loop over our data iterator, and feed the inputs to the
-# network and optimize.
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+# Training
+for itr in range(max_iters):
+    avg_loss = 0
+    avg_acc = 0
+    for i, data in enumerate(train_loader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-
+        _, answers = torch.max(labels, dim=1)
         # zero the parameter gradients
         optimizer.zero_grad()
-
-        # forward + backward + optimize
+        # forward
         outputs = net(inputs)
-        loss = criterion(outputs, labels)
+        # Accuracy
+        _, predictions = torch.max(outputs, dim=1)
+        avg_acc += torch.count_nonzero(predictions == answers).item()
+        # Loss
+        loss = criterion(outputs, answers)
+        avg_loss += loss.item()
+        #  backward + optimize
         loss.backward()
         optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+    # Average loss and accuracy over training set
+    avg_acc /= train_x.shape[0]
+    avg_loss /= train_x.shape[0]
+    print('Acc/loss = {} / {}'.format(avg_acc, avg_loss))
 
 print('Finished Training')
 
